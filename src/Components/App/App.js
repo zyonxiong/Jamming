430baf5a498155b209ec060719ccd7a29eacea04
Zyon Xiong <xiong.houa95@gmail.com>
2022-03-10 13:10:49 -0600

    Added spotify API to complete web application.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,13 +11,16 @@ class App extends React.Component {
       //to add states to our component
 
     this.state = {
-      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
-                      {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-                      {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
+      searchResults: [],
+                    //  [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+                    //   {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+                    //   {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
       playlistName: 'My Playlist',
-      playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4},
-      {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5},
-      {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
+      playlistTracks: []
+      
+      // [{name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4},
+      // {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5},
+      // {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -49,11 +53,19 @@ class App extends React.Component {
     // alert("This method is linked to the button.")
     //click on Save to Spotify button on web app
     const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks : []
+      })
+    })
   }
 
   search(term) {
 
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({ searchResults: searchResults })
+    });
   }
 
   render() {
